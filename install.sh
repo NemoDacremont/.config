5450efe573e2b2a72286456d2515d390c617eed7
Nemo D'acremont <ndacremont@enseirb-matmeca.fr>
2024-02-03 22:37:52 +0100

    more things in install script

--- install.sh
+++ install.sh
@@ -1,38 +1,91 @@
-#!/usr/bin/sh
+#!/bin/sh
 
-function install_i3_conf() {
-	cp -r i3 $HOME/.config/i3
+script_pos=$PWD
+
+# Make backup file for every files given in argument
+backup() {
+	for bak in $@
+	do
+		[ -f $bak ] && echo $bak file exists, creating $bak.bak backup && cp $bak $bak.bak
+		[ -d $bak ] && echo $bak directory exists, creating $bak.bak backup && cp -r $bak $bak.bak
+	done
+
+	return 0
 }
 
-function install_nvim_conf() {
-	cp -r nvim $HOME/.config/i3
+
+install_i3_conf() {
+	mkdir -p $HOME/.config
+	backup $HOME/.config/i3 && cp -r i3 $HOME/.config
 }
 
 
-function installX_conf() {
-	echo Copy xorg to /etc/X11
-	sudo cp -r xorg /etc/X11
-
-	cp .xinitrc $HOME
+install_nvim_conf() {
+	mkdir -p $HOME/.config
+	backup $HOME/.config/nvim && cp -r nvim $HOME/.config
 }
 
-function install_profile() {
-	cp .profile $HOME
+
+installX_conf() {
+	echo Copying next files to /etc/X11/xorg.conf.d :
+	ls xorg
+	echo sudo is required for the next command : `sudo backup /etc/X11/xorg.conf.d && cp -r xorg /etc/X11/xorg.conf.d`
+
+	sudo backup /etc/X11/xorg.conf.d && cp -r xorg /etc/X11/xorg.conf.d
+
+	backup $HOME/.xinitrc && cp .xinitrc $HOME
 }
 
-function install_shell() {
-	cp -r .shell $HOME
+
+install_profile() {
+	backup $HOME/.profile && cp .profile $HOME
 }
 
-function install_polybar_conf() {
+
+install_shell() {
+	backup $HOME/.shell && cp -r .shell $HOME
+}
+
+
+install_polybar_conf() {
+	mkdir -p $HOME/.config
 	cp -r polybar $HOME/.config
 }
 
-function install_scripts() {
+
+install_scripts() {
 	mkdir -p $HOME/.local/bin
 	cp my_scripts/* $HOME/.local/bin
 }
 
 
+install_picom_config() {
+	mkdir -p $HOME/.config
+	cp picom.conf $HOME/.config/picom.conf
+}
+
+
+install_st() {
+	mkdir -p $HOME/.local/srcs
+	cd $HOME/.local/srcs
+	git clone https://github.com/NemoDacremont/st.git
+	cd st
+
+	make clean install DESTDIR=$HOME/.local
+	cd $script_pos
+}
+
+
+install_dwm() {
+	mkdir -p $HOME/.local/srcs
+	cd $HOME/.local/srcs
+	git clone https://github.com/NemoDacremont/dwm.git
+	cd dwm
+
+	make clean install DESTDIR=$HOME/.local
+	cd $script_pos
+}
+
+install_profile
 
 
